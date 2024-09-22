3dc68ac8c50a80f84db082d2fc4179ad411c8926
prakharpandeyyy <8.prakharpandey@gmail.com>
2024-09-22 17:31:13 +0530

    vercel

--- backend/app.js
+++ backend/app.js
@@ -3,7 +3,11 @@ const bodyParser = require("body-parser");
 
 const cors = require("cors");
 const app = express();
-app.use(cors());
+app.use(cors({
+  origin: "*",
+  methods: "GET,POST",
+
+}));
 const port = process.env.PORT || 3000;
 
 app.use(bodyParser.json({ limit: "10mb" }));
